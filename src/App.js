ee0f9bdcae7b7a794363c4e22f24aedaa9ebfdbd
kang-mincheol <aaa6315@gmail.com>
2023-01-11 10:08:25 +0900

    헤더 작업중

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
-import Header from "./Header";
+import "./App.css";
+// import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <div className="Header">
+        <div className="logo_wrap">
+          <a className="home_btn" href="/">
+            민집사
+            {/* <img className="minzipsa_logo" src=""/> */}
+          </a>
+        </div>
+
+        <div className="menu_wrap">
+
+        </div>
+
+        <div className="util_wrap">
+          <button className="hamburger_btn"></button>
+        </div>
+      </div>
     </div>
   );
 }
